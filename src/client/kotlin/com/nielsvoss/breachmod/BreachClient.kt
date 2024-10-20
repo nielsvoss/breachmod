c29d6d02c099ffd8cbcb9c9937841d9426d457a6
Niels Voss <code@nielsvoss.com>
2024-10-19 17:28:17 -0700

    Create mod template using Fabric CLI

---
+++ src/client/kotlin/com/nielsvoss/breachmod/BreachClient.kt
@@ -0,0 +1,9 @@
+package com.nielsvoss.breachmod
+
+import net.fabricmc.api.ClientModInitializer
+
+object BreachClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
