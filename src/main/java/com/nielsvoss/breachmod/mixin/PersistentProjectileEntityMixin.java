6a596a4525461e62a5d85322d181194a3ac1b261
Niels Voss <code@nielsvoss.com>
2024-10-26 00:22:03 -0700

    Implement instant-kill arrows using mixins

---
+++ src/main/java/com/nielsvoss/breachmod/mixin/PersistentProjectileEntityMixin.java
@@ -0,0 +1,28 @@
+package com.nielsvoss.breachmod.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(PersistentProjectileEntity.class)
+abstract class PersistentProjectileEntityMixin extends ProjectileEntity {
+	public PersistentProjectileEntityMixin(EntityType<? extends ProjectileEntity> entityType, World world) {
+		super(entityType, world);
+	}
+
+	@WrapOperation(method = "onEntityHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"))
+	private boolean increaseDamage(Entity instance, DamageSource source, float amount, Operation<Boolean> original) {
+		if (this.getType() == EntityType.ARROW) {
+			float newDamageAmount = 1_000_000_000.0F; // Arbitrary large number
+			return original.call(instance, source, newDamageAmount);
+		}
+		return original.call(instance, source, amount);
+	}
+}
