85501d82da3cdb822ea0492d091b26890b9f6233
Niels Voss <code@nielsvoss.com>
2024-11-08 02:14:01 -0800

    Create sidebar in BreachActive

--- src/main/kotlin/com/nielsvoss/breachmod/BreachActive.kt
+++ src/main/kotlin/com/nielsvoss/breachmod/BreachActive.kt
@@ -1,5 +1,6 @@
 package com.nielsvoss.breachmod
 
+import eu.pb4.sidebars.api.Sidebar
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.server.world.ServerWorld
 import net.minecraft.text.Text
@@ -9,6 +10,7 @@ import xyz.nucleoid.plasmid.game.common.team.TeamManager
 import xyz.nucleoid.plasmid.game.event.GameActivityEvents
 import xyz.nucleoid.plasmid.util.PlayerRef
 
+// Designed similarly to https://github.com/NucleoidMC/skywars/blob/1.20/src/main/java/us/potatoboy/skywars/game/SkyWarsActive.java
 class BreachActive private constructor(private val gameSpace: GameSpace, private val world: ServerWorld,
                    private val config: BreachGameConfig, private val attackingTeam: GameTeam,
                    private val defendingTeam: GameTeam, private val teamManager: TeamManager) {
@@ -29,11 +31,14 @@ class BreachActive private constructor(private val gameSpace: GameSpace, private
                 val breachActive = BreachActive(gameSpace, world, config, attackingTeam, defendingTeam, teamManager)
 
                 activity.listen(GameActivityEvents.TICK, GameActivityEvents.Tick { breachActive.tick() })
+
+                breachActive.start()
             }
         }
     }
 
-    val roundTimer: BreachRoundTimer
+    private val gameSidebar = Sidebar(Sidebar.Priority.MEDIUM)
+    private val roundTimer: BreachRoundTimer
     init {
         var prepTicks = config.prepLengthInSeconds * 20;
         var roundTicks = config.roundLengthInSeconds * 20;
@@ -46,12 +51,31 @@ class BreachActive private constructor(private val gameSpace: GameSpace, private
             roundTicks = 1
         }
         roundTimer = BreachRoundTimer(prepTicks, roundTicks)
+
+        buildSidebar()
+    }
+
+    private fun buildSidebar() {
+        gameSidebar.title = Text.translatable("breach.sidebar.title")
+        gameSidebar.set {
+            it.add { _ ->
+                Text.literal(roundTimer.displayTime())
+            }
+        }
     }
 
     private fun tick() {
         val phaseThatJustEnded: BreachRoundTimer.Phase? = roundTimer.tick()
         if (phaseThatJustEnded != null) {
             broadcast(Text.literal("Phase just ended"))
+        }
+    }
+
+    private fun start() {
+        gameSidebar.show()
+        // TODO: Handle offline players
+        for (player in onlineParticipants()) {
+            gameSidebar.addPlayer(player)
         }
     }
 
