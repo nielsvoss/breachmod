5de581acf9d0fa1963a0000b7f823a55e10d5125
Niels Voss <code@nielsvoss.com>
2024-11-11 17:03:00 -0800

    Load targets from map metadata

--- src/main/kotlin/com/nielsvoss/breachmod/BreachMap.kt
+++ src/main/kotlin/com/nielsvoss/breachmod/BreachMap.kt
@@ -3,20 +3,31 @@ package com.nielsvoss.breachmod
 import net.minecraft.server.MinecraftServer
 import net.minecraft.text.Text
 import net.minecraft.util.Identifier
+import net.minecraft.util.math.BlockPos
 import xyz.nucleoid.map_templates.MapTemplate
 import xyz.nucleoid.map_templates.MapTemplateSerializer
 import xyz.nucleoid.map_templates.TemplateRegion
 import xyz.nucleoid.plasmid.game.GameOpenException
 import xyz.nucleoid.plasmid.game.world.generator.TemplateChunkGenerator
 
-class BreachMap private constructor(private val template: MapTemplate, val lobbySpawnRegion: TemplateRegion) {
+class BreachMap private constructor(private val template: MapTemplate, val lobbySpawnRegion: TemplateRegion,
+                                    val targets: List<BreachTarget>) {
     companion object {
         @Throws(GameOpenException::class)
         fun load(mapId: Identifier, server: MinecraftServer): BreachMap {
             val template: MapTemplate = MapTemplateSerializer.loadFromResource(server, mapId)
             val lobbySpawnRegion = template.metadata.getFirstRegion("lobbySpawn")
                 ?: throw GameOpenException(Text.literal("Provided map did not contain a lobbySpawn region"))
-            return BreachMap(template, lobbySpawnRegion)
+
+            val targetBlocks = mutableListOf<BlockPos>()
+            for (region in template.metadata.getRegions("target")) {
+                if (region.bounds.min != region.bounds.max) {
+                    throw GameOpenException(Text.literal("A target region is not 1x1x1"))
+                }
+                targetBlocks.add(region.bounds.min)
+            }
+            val targets: List<BreachTarget> = targetBlocks.map { BreachTarget(it, template.getBlockState(it).block) }
+            return BreachMap(template, lobbySpawnRegion, targets)
         }
     }
 
---
+++ src/main/kotlin/com/nielsvoss/breachmod/BreachTarget.kt
@@ -0,0 +1,6 @@
+package com.nielsvoss.breachmod
+
+import net.minecraft.block.Block
+import net.minecraft.util.math.BlockPos
+
+data class BreachTarget(val pos: BlockPos, val block: Block)
