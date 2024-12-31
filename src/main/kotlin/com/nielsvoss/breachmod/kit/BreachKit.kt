d9f4bec0122f431b39e7f466f8ad2f03cbe7d9d0
Niels Voss <code@nielsvoss.com>
2024-12-31 12:37:15 -0800

    Start working on kits

---
+++ src/main/kotlin/com/nielsvoss/breachmod/kit/BreachKit.kt
@@ -0,0 +1,28 @@
+package com.nielsvoss.breachmod.kit
+
+import com.mojang.serialization.Codec
+import com.mojang.serialization.codecs.RecordCodecBuilder
+import net.minecraft.item.ItemStack
+import net.minecraft.server.network.ServerPlayerEntity
+import xyz.nucleoid.codecs.MoreCodecs
+import xyz.nucleoid.plasmid.util.ItemStackBuilder
+
+/**
+ * Originally based on https://github.com/NucleoidMC/skywars/blob/main/src/main/java/us/potatoboy/skywars/kit/Kit.java
+ */
+@JvmRecord
+data class BreachKit(val items: List<ItemStack>) {
+    companion object {
+        @JvmStatic
+        val CODEC: Codec<BreachKit> = RecordCodecBuilder.create{ instance ->
+            instance.group(Codec.list(MoreCodecs.ITEM_STACK).fieldOf("items").forGetter(BreachKit::items))
+                .apply(instance, ::BreachKit)
+        }
+    }
+
+    fun equipPlayer(player: ServerPlayerEntity) {
+        for (stack in items) {
+            player.inventory.insertStack(ItemStackBuilder.of(stack).build())
+        }
+    }
+}
---
+++ src/main/kotlin/com/nielsvoss/breachmod/kit/BreachKitRegistry.kt
@@ -0,0 +1,18 @@
+package com.nielsvoss.breachmod.kit
+
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper
+import net.minecraft.resource.ResourceType
+import xyz.nucleoid.plasmid.registry.TinyRegistry
+
+/**
+ * Originally based on
+ * https://github.com/NucleoidMC/skywars/blob/main/src/main/java/us/potatoboy/skywars/kit/KitRegistry.java
+ */
+object BreachKitRegistry {
+    val KITS: TinyRegistry<BreachKit> = TinyRegistry.create()
+
+    fun register() {
+        val serverData: ResourceManagerHelper = ResourceManagerHelper.get(ResourceType.SERVER_DATA)
+        TODO()
+    }
+}
