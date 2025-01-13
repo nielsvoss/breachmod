d9ec84df490c93a2c6bb242e3016ed53633a8859
Niels Voss <code@nielsvoss.com>
2025-01-12 17:23:14 -0800

    Restore old ui after closing kit selection ui
    Previously, when the kit selection ui was opened, it would clear your
    waiting lobby ui and prevent you from opening the kit selection again.
    
    Based on https://github.com/NucleoidMC/skywars/commit/c119383f2da101b254e35f4cabe4c975491d40b8#diff-8e062be0090d50f941620c533b0c0477e434148d75105ac35700c8656f5862a8R62

--- src/main/kotlin/com/nielsvoss/breachmod/ui/KitSelectorUI.kt
+++ src/main/kotlin/com/nielsvoss/breachmod/ui/KitSelectorUI.kt
@@ -3,7 +3,9 @@ package com.nielsvoss.breachmod.ui;
 import com.nielsvoss.breachmod.kit.BreachKit
 import com.nielsvoss.breachmod.kit.BreachKitRegistry
 import com.nielsvoss.breachmod.util.UIUtils
+import eu.pb4.sgui.api.GuiHelpers
 import eu.pb4.sgui.api.elements.GuiElementBuilder
+import eu.pb4.sgui.api.gui.GuiInterface
 import eu.pb4.sgui.api.gui.SimpleGui
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text
@@ -13,11 +15,13 @@ class KitSelectorUI private constructor(
     player: ServerPlayerEntity,
     private val kits: List<BreachKit>,
     private val titleText: Text,
+    private val prev: GuiInterface?,
     private val onSelectKit: BiConsumer<KitSelectorUI, BreachKit>
 ) : SimpleGui(UIUtils.getTypeOrThrowIfTooLarge(kits.size), player, false) {
     companion object {
         fun open(player: ServerPlayerEntity, kits: List<BreachKit>, title: Text, onSelectKit: BiConsumer<KitSelectorUI, BreachKit>): Boolean {
-            val ui = KitSelectorUI(player, kits, title, onSelectKit)
+            val prev: GuiInterface? = GuiHelpers.getCurrentGui(player)
+            val ui = KitSelectorUI(player, kits, title, prev, onSelectKit)
             return ui.open()
         }
     }
@@ -36,5 +40,11 @@ class KitSelectorUI private constructor(
         }
 
         super.onOpen() // Might not do anything right now?
+    }
+
+    override fun onClose() {
+        prev?.open()
+
+        super.onClose() // Not sure if this is needed
     }
 }
