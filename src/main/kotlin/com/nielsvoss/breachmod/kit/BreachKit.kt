dbbdb364d37401482a7baa184e75fe8f296c8949
Niels Voss <code@nielsvoss.com>
2025-01-17 21:17:19 -0800

    Use Optional in kit armor and add offhand option

--- src/main/kotlin/com/nielsvoss/breachmod/kit/BreachKit.kt
+++ src/main/kotlin/com/nielsvoss/breachmod/kit/BreachKit.kt
@@ -4,18 +4,17 @@ import com.mojang.serialization.Codec
 import com.mojang.serialization.codecs.RecordCodecBuilder
 import net.minecraft.entity.EquipmentSlot
 import net.minecraft.item.ItemStack
-import net.minecraft.item.Items
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.text.Text
 import xyz.nucleoid.codecs.MoreCodecs
-import xyz.nucleoid.plasmid.api.util.ItemStackBuilder
+import java.util.*
 
 /**
  * Originally based on https://github.com/NucleoidMC/skywars/blob/main/src/main/java/us/potatoboy/skywars/kit/Kit.java
  */
 @JvmRecord
 data class BreachKit(val nameTranslationKey: String, private val icon: ItemStack, val items: List<ItemStack>,
-                     val armor: KitArmor, val categories: List<String>) {
+                     val armor: KitArmor, val offhand: Optional<ItemStack>, val categories: List<String>) {
     companion object {
         @JvmStatic
         val CODEC: Codec<BreachKit> = RecordCodecBuilder.create{ instance ->
@@ -23,21 +22,24 @@ data class BreachKit(val nameTranslationKey: String, private val icon: ItemStack
                 Codec.STRING.fieldOf("name").forGetter(BreachKit::nameTranslationKey),
                 MoreCodecs.ITEM_STACK.fieldOf("icon").forGetter(BreachKit::icon),
                 Codec.list(MoreCodecs.ITEM_STACK).fieldOf("items").forGetter(BreachKit::items),
-                KitArmor.CODEC.fieldOf("armor").forGetter(BreachKit::armor),
+                KitArmor.CODEC.optionalFieldOf("armor", KitArmor.EMPTY).forGetter(BreachKit::armor),
+                MoreCodecs.ITEM_STACK.optionalFieldOf("offhand").forGetter(BreachKit::offhand),
                 Codec.list(Codec.STRING).fieldOf("categories").forGetter(BreachKit::categories)
             ).apply(instance, ::BreachKit)
         }
     }
 
     fun equipPlayer(player: ServerPlayerEntity) {
-        for (stack in items) {
-            player.inventory.insertStack(ItemStackBuilder.of(stack).build())
+        for ((i, stack) in items.withIndex()) {
+            player.inventory.insertStack(i, stack.copy())
         }
 
-        if (!armor.helmet.isOf(Items.AIR)) player.equipStack(EquipmentSlot.HEAD, armor.helmet.copy())
-        if (!armor.chestplate.isOf(Items.AIR)) player.equipStack(EquipmentSlot.BODY, armor.chestplate.copy())
-        if (!armor.leggings.isOf(Items.AIR)) player.equipStack(EquipmentSlot.LEGS, armor.leggings.copy())
-        if (!armor.boots.isOf(Items.AIR)) player.equipStack(EquipmentSlot.FEET, armor.boots.copy())
+        if (armor.helmet.isPresent) player.equipStack(EquipmentSlot.HEAD, armor.helmet.get().copy())
+        if (armor.chestplate.isPresent) player.equipStack(EquipmentSlot.BODY, armor.chestplate.get().copy())
+        if (armor.leggings.isPresent) player.equipStack(EquipmentSlot.LEGS, armor.leggings.get().copy())
+        if (armor.boots.isPresent) player.equipStack(EquipmentSlot.FEET, armor.boots.get().copy())
+
+        if (offhand.isPresent) player.equipStack(EquipmentSlot.OFFHAND, offhand.get().copy())
     }
 
     fun getIcon(): ItemStack {
--- src/main/kotlin/com/nielsvoss/breachmod/kit/KitArmor.kt
+++ src/main/kotlin/com/nielsvoss/breachmod/kit/KitArmor.kt
@@ -5,18 +5,23 @@ import com.mojang.serialization.codecs.RecordCodecBuilder
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
 import xyz.nucleoid.codecs.MoreCodecs
+import java.util.*
 
 @JvmRecord
-data class KitArmor(val helmet: ItemStack, val chestplate: ItemStack, val leggings: ItemStack, val boots: ItemStack) {
+data class KitArmor(val helmet: Optional<ItemStack>, val chestplate: Optional<ItemStack>,
+                    val leggings: Optional<ItemStack>, val boots: Optional<ItemStack>) {
     companion object {
         @JvmStatic
         val CODEC: Codec<KitArmor> = RecordCodecBuilder.create{ instance ->
             instance.group(
-                MoreCodecs.ITEM_STACK.optionalFieldOf("helmet", ItemStack(Items.AIR)).forGetter(KitArmor::helmet),
-                MoreCodecs.ITEM_STACK.optionalFieldOf("chestplate", ItemStack(Items.AIR)).forGetter(KitArmor::chestplate),
-                MoreCodecs.ITEM_STACK.optionalFieldOf("leggings", ItemStack(Items.AIR)).forGetter(KitArmor::leggings),
-                MoreCodecs.ITEM_STACK.optionalFieldOf("boots", ItemStack(Items.AIR)).forGetter(KitArmor::boots)
+                MoreCodecs.ITEM_STACK.optionalFieldOf("helmet").forGetter(KitArmor::helmet),
+                MoreCodecs.ITEM_STACK.optionalFieldOf("chestplate").forGetter(KitArmor::chestplate),
+                MoreCodecs.ITEM_STACK.optionalFieldOf("leggings").forGetter(KitArmor::leggings),
+                MoreCodecs.ITEM_STACK.optionalFieldOf("boots").forGetter(KitArmor::boots)
             ).apply(instance, ::KitArmor)
         }
+
+        @JvmStatic
+        val EMPTY = KitArmor(Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty())
     }
 }
